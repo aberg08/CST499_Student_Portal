f5bf2e2143bbb6aa2828f28adcb17ebf13ab3718
aberg08 <33042345+aberg08@users.noreply.github.com>
2021-11-11 21:21:24 -0700

    Add files via upload

---
+++ code/student-portal/connectToDB.php
@@ -0,0 +1,12 @@
+<?php
+
+$serverName = "localhost";
+$userName= "root";
+$password = "";
+$dbName="cst499";
+
+//create Connection
+
+$con = mysqli_connect($serverName,$userName,$password,$dbName);
+
+?>
